611747786d5b75343144cb10e3bda947533ec192
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-01-15 08:12:37 -0300

    feat(Rotas): Criado primeiras rotas com node.js base

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+  "/": "Curso de Node.js",
+  "/express": "Curso de Express",
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "content-type": "text/plain" });
-  res.end("Curso de Node.js");
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
-  console.log("Servidor escutando!");
+  console.log("Servidor escutando! http://localhost:3000");
 });
